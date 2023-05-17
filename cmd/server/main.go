7f404a8f0c6227dfa88af2aa3e9046c830bc6718
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2023-05-17 14:21:10 +0700

    fix wasm and fasthttp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/valyala/fasthttp"
 )
 
 func main() {
+	fmt.Println("WebAssembly Run!")
 	log.Fatal(fasthttp.ListenAndServe(":8080", fasthttp.FSHandler("../../assets", 0)))
 }
