2df41dc53bce02180729611ba78d3cb9d2c044f1
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2023-05-17 14:21:52 +0700

    fix wasm and fasthttp

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,4 @@ const goWasm = new Go()
 WebAssembly.instantiateStreaming(fetch("main.wasm"), goWasm.importObject)
     .then((result) => {
         goWasm.run(result.instance)
-
-        document.getElementById("get-html").addEventListener("click", ()=> {
-            document.body.innerHTML+=getHtml()
-        })
     })
